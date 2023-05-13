29d0bc379fed21f866e4b600d2737c4731ddba41
shree-ya <44109067+shree-ya@users.noreply.github.com>
2023-05-13 19:45:31 -0400

    Cosine Similarity

---
+++ gptscript_clustering.py
@@ -0,0 +1,117 @@
+#from torch.utils.data import Dataset
+#from torch.utils.data import Dataset, DataLoader
+import os
+import json
+import csv
+#import pandas as pd
+
+class ABGCoQADataset():
+    def __init__(self, data_file, tokenizer):
+        super().__init__()
+        self.dataset = self._build_data(data_file)
+        self.contexts = [d[0] for d in self.dataset]
+        self.cqs = [d[1] for d in self.dataset]
+        # encodings = tokenizer.prepare_seq2seq_batch(self.contexts, self.cqs, max_length=512, padding=True, truncation=True, return_tensors="pt")
+        # self.encoder_input_ids = encodings.input_ids
+        # self.encoder_attention_mask = encodings.attention_mask
+        # self.decoder_input_ids = encodings.labels[:, :-1].clone()  # skip last
+        # self.labels = encodings.labels[:, 1:].clone()              # skip first
+
+    def _build_data(self, data_file):
+        dataset = []
+        with open(f"data/{data_file}") as f:
+            coqa_data = json.load(f)
+
+        # create a dict of dicts by id
+        coqa_data_dict = {d['id']: d for d in coqa_data['data']}
+
+        # if 'train' in data_file:
+        #     for i in range(15):
+        #         with open(f"data/output_{i}.json") as f:
+        #             output_data = json.load(f)
+        #             for output in output_data:
+        #                 story = coqa_data_dict[output['id']]['story']
+        #                 ambiguous_question = coqa_data_dict[output['id']]['target_turn']['question']
+        #                 history_turns = '\n'.join([ f"{turn['question']}\n{turn['answer']}" for turn in coqa_data_dict[output['id']]['history_turns']])
+        #                 clarification_question_list = [coqa_data_dict[output['id']]['clarification_turn']['question']]
+
+        #                 if output['new_questions'].find('Clarifying Questions') != -1:
+        #                     output['new_questions'] = output['new_questions'][output['new_questions'].find('Clarifying Questions') + len('Clarifying Questions'):]
+
+        #                 start = -1
+        #                 for index, letter in enumerate(output['new_questions']):
+        #                     if letter.isupper() and start == -1:
+        #                         start = index
+        #                     elif letter == '?':
+        #                         end = index
+        #                         clarification_question_list.append(output['new_questions'][start: end + 1])
+        #                         start = -1
+
+        #                 for cq in clarification_question_list:
+        #                     dataset.append([f"Story:\n{story}\nHistory:\n{history_turns}\nAmbiguous Question:\n{ambiguous_question}\nClarifying Question:", cq])
+
+        # else:
+
+        for data in coqa_data['data']:
+            if data['ambiguity'] == 'ambiguous':
+                story = data['story']
+                ambiguous_question = data['target_turn']['question']
+                history_turns = '\n'.join([ f"{turn['question']}\n{turn['answer']}" for turn in data['history_turns']])
+                clarification_question = data['clarification_turn']['question']
+
+                dataset.append([f"Story:\n{story}\nHistory:\n{history_turns}\nAmbiguous Question:\n{ambiguous_question}\nClarifying Question:", clarification_question])
+
+        return dataset
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, item):
+        return self.encoder_input_ids[item], self.encoder_attention_mask[item], self.decoder_input_ids[item], self.labels[item] 
+
+
+from sentence_transformers import SentenceTransformer, util
+import torch
+import json
+
+if __name__ == "__main__":
+    data_obj = ABGCoQADataset('coqa_abg_test.json', 'test')
+    # print(data_obj.dataset[41][0])
+    # print(data_obj.dataset[41][1])
+
+    embedder = SentenceTransformer('all-MiniLM-L6-v2')
+
+    corpus = [d[0] for d in data_obj.dataset]
+    cqs = [d[1] for d in data_obj.dataset]
+    corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
+
+    result_dict = {}
+    top_k = 3  # Define top_k here
+
+    for query in data_obj.dataset:
+        story = query[0].split('\n')[1]
+        history_turns = query[0].split('\n')[3]
+        ambiguous_question = query[0].split('\n')[5]
+
+        query_text = f"Story:\n{story}\nHistory:\n{history_turns}\nAmbiguous Question:\n{ambiguous_question}"
+
+        query_embedding = embedder.encode(query_text, convert_to_tensor=True)
+        cos_scores = util.cos_sim(query_embedding, corpus_embeddings)[0]
+        top_results = torch.topk(cos_scores, k=top_k + 1)  # Add 1 to exclude the key itself
+
+        key = query_text + " Clarifying Question: "
+        values = []
+
+        for score, idx in zip(top_results[0], top_results[1]):
+            if corpus[idx] != key:
+                values.append((corpus[idx] + cqs[idx], score.item()))
+
+        values = values[1:3]  # Keep only the 2nd and 3rd highest scores
+
+        result_dict[key] = values
+
+    # Save the results in a JSON file
+    with open('result_dict.json', 'w') as f:
+        json.dump(result_dict, f, indent=4)
+
+    print("Results saved in result_dict.json")
